3d27156887d2f15427a7a32f7cb3b8aa6c31e7c0
saulgoodman0111 <130331638+saulgoodman0111@users.noreply.github.com>
2023-07-14 08:15:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+let a=0, b=1;
 function fibonacci(num) {
-// your code here
+    if(num <= 2)
+    return num-1;
+    for(let i=1;i<=num-2;i++)
+    {
+        let temp=b;
+        b=a+b;
+        a=temp;
+    }
+    return b;
 }
-
+// your code here
 module.exports = fibonacci;
